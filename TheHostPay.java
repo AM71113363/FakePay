347580141db1cdc279f3e6c98df3e60471945472
AM71113363 <71590903+AM71113363@users.noreply.github.com>
2025-03-12 10:36:36 +0100

    Update TheHostPay.java

--- TheHostPay.java
+++ TheHostPay.java
@@ -7,17 +7,12 @@ import android.os.*;
 
 public class TheHostPay extends HostApduService
 {
-
-	@Override
-	public byte[] processCommandApdu(byte[] p1, Bundle p2)
-	{
-		// TODO: Implement this method
-		return new byte[]{ (byte)0x90,(byte)0x00};
-	}
-
-	@Override
-	public void onDeactivated(int p1)
-	{
-	}
-
+    @Override
+    public byte[] processCommandApdu(byte[] p1, Bundle p2)
+    {
+	return new byte[]{ (byte)0x90,(byte)0x00};
+    }
+    @Override
+    public void onDeactivated(int p1)
+    { }
 }
